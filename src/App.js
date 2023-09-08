11edb0afef068610e3c072498b5ec0e692a140a3
drelarson <larsonandre01@gmail.com>
2023-09-08 09:21:00 -0700

    created skills section

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import Landing from './Components/Landing/Landing';
 import Navbar from './Components/Navs/Navbar';
 import AboutMe from './Components/About/AboutMe'
 import Projects from './Components/Projects/Projects';
+import Skills from './Components/Skills/Skills';
 
 function App() {
   return (
     <div id="app">
       <Navbar />
       <div id="wholeLanding">
+        
         <Landing />
         <AboutMe/>
         <Projects/>
+        <Skills/>
 
 
       </div>
